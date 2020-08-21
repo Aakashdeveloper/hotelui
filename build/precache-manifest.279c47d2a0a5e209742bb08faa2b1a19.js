f6ec4f5ccecd16fc5b3d89da5db3b337d53dda65
aakashdeveloper <ahanda205@gmail.com>
2020-08-21 17:07:15 +0530

    a

---
+++ build/precache-manifest.279c47d2a0a5e209742bb08faa2b1a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315c2e1ee51d9e727559db604facba03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a0ee612faaf07047e9",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "643eafc51d6b6bed962b",
+    "url": "/static/css/main.440d6f43.chunk.css"
+  },
+  {
+    "revision": "49a0ee612faaf07047e9",
+    "url": "/static/js/2.02debdac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02debdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643eafc51d6b6bed962b",
+    "url": "/static/js/main.136dc497.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
